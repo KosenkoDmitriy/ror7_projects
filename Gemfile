e098fef8000dd4098be49dd220a213cefaef6ebf
Dmitry K <kosenkodmitryv@gmail.com>
2023-01-25 17:42:57 +0300

    rspec setup

--- Gemfile
+++ Gemfile
@@ -23,16 +23,20 @@ gem 'vite_rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "faker"
 end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  gem "spring"
+  gem "spring-watcher-listen"
+  gem "sqlite3", "~> 1.6.0"
 end
 
 gem "devise", "~> 4.8"
 gem "jsonapi-serializer", "~> 2.2"
 
-group :development do
-  gem "sqlite3", "~> 1.6.0"
+group :test do
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,27 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @attrs = { email: 'user@ex.com' }
+  end
+  test "Allow the user to create one or more projects" do
+    # user = User.new email: 'user@ex.com'
+    user = User.create email: 'user@ex.com'
+  #   project_data = [
+  #     no: '#1',
+  #     title: 'test project',
+  #     descrtiption: 'test description',
+  #     date_created: '',
+  #     users: [user], #assigned to
+  #     status: 'active' #todo: enum
+  # ]
+  # user.projects << Project.new(project_data)
+  # user.projects << Project.new(project_data)
+  # assert_equal user.projects.count, 1
+  assert_equal user.email, 'user@ex.com'
+  end
+# User can get an overview of all the projects
+# • For each project the user can create one or more issues
+# The user can get an overview of all issues on a project 
+# • The issues can be created, edited and commented by the users
 end
