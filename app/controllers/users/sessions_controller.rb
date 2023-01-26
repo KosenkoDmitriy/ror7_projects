f213d3a1e356c59bbf628e9e1f9a0154f22f20e6
Dmitry K <kosenkodmitryv@gmail.com>
2023-01-26 23:55:43 +0300

    Logout with CSRF verification issue

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,10 +7,10 @@ class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
   
   # GET /resource/sign_in
-  def new
-    super
-  end
-
+  # def new
+  #   super
+  # end
+  
   # POST /resource/sign_in
   def create
     super
@@ -24,10 +24,17 @@ class Users::SessionsController < Devise::SessionsController
   private
   
   def respond_with(resource, _opts = {})
-    render json: {
-      status: {code: 200, message: 'Logged in successfully.'},
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
+    if resource.present?
+      render json: {
+        status: {code: 200, message: 'Logged in successfully.'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "You need to sign in or sign up before continuing."
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
--- app/javascript/actions/userAction.jsx
+++ app/javascript/actions/userAction.jsx
@@ -1,4 +1,5 @@
 const userLogin = async (form_data) => {
+  console.log('userLogin', form_data);
   const json_data = JSON.stringify({user: form_data});
   const requestOptions = {
     method: 'POST',
@@ -16,6 +17,7 @@ const userLogin = async (form_data) => {
 }
 
 const userSignup = async (form_data) => {
+  console.log('userSignup', form_data);
   const json_data = JSON.stringify({user: form_data});
   const requestOptions = {
     method: 'POST',
@@ -31,4 +33,22 @@ const userSignup = async (form_data) => {
   const res = await fetch("/sign_up", requestOptions).then(response => response.json())
   return res;
 }
-export { userLogin, userSignup };
+
+const userLogout = async (form_data) => {
+  console.log('userLogout', form_data);
+  const json_data = JSON.stringify({sign_in: form_data});
+  const requestOptions = {
+    method: 'DELETE',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").getAttribute("content"),
+    },
+    body: json_data
+  };
+
+  const res = await fetch("/logout", requestOptions).then(response => response.json())
+  return res;
+}
+export { userLogin, userSignup, userLogout };
